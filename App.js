239cb5f15e81b89c308f639fb5cb80aabdeea270
milandetruja <milandetruja2@gmail.com>
2024-02-19 23:12:00 +0530

    login screen ui create

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/screen/Login';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'poppinsMedium': require('./src/assets/fonts/Poppins-Medium.ttf'),
+    'poppinsLight': require('./src/assets/fonts/Poppins-Light.ttf'),
+    'poppinsRegular': require('./src/assets/fonts/Poppins-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1" onLayout={onLayoutRootView}>
+     <Login/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/components/screen/Login.js
@@ -0,0 +1,101 @@
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TextInput,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+
+const socialData = [
+  {
+    image: require("../../../src/assets/images/facebook.png"),
+  },
+  {
+    image: require("../../../src/assets/images/google.png"),
+  },
+  {
+    image: require("../../../src/assets/images/iso.png"),
+  },
+];
+
+const Login = () => {
+  return (
+    <View>
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor="#fff"
+      />
+      <View className="justify-center items-center">
+        <Image
+          source={require("../../../src/assets/images/Logo.png")}
+          className="w-[160px] h-[146px] mt-6 "
+        />
+      </View>
+      <Text
+        style={{ fontFamily: "poppinsRegular" }}
+        className="text-center text-[32px] pt-7"
+      >
+        Welcome back!
+      </Text>
+      <View className="pt-12 px-8">
+        <View className="border-2 rounded-[10px]">
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#000"
+            className="px-5 py-2"
+            style={{ fontFamily: "poppinsMedium" }}
+          />
+        </View>
+        <View className="border-2 rounded-[10px] mt-5">
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#000"
+            className="px-5 py-2 "
+            secureTextEntry={true}
+            style={{ fontFamily: "poppinsMedium" }}
+          />
+        </View>
+        <View className="justify-end items-end pt-4 ">
+          <Text style={{ fontFamily: "poppinsRegular" }} className="text-base">
+            Forgot password?
+          </Text>
+        </View>
+      </View>
+      <View className="pt-[53px] px-8">
+        <TouchableOpacity className="bg-[#0098FF] justify-center items-center rounded-[10px]">
+          <Text
+            style={{ fontFamily: "poppinsMedium" }}
+            className="py-5 text-white text-lg"
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className=" justify-center items-center px-8 py-4">
+        <Text style={{ fontFamily: "poppinsRegular" }} className="text-xl">
+          or
+        </Text>
+      </View>
+      <View className="px-8">
+        <View className="flex-row justify-between items-center">
+          {socialData.map((val, index) => (
+            <View className="border-2 p-4 rounded-[10px]" key={index}>
+              <Image source={val.image} className="w-8 h-8 " />
+            </View>
+          ))}
+        </View>
+      </View>
+      <View className=" justify-center items-center px-8 py-10">
+        <Text style={{ fontFamily: "poppinsRegular" }} className="text-xl">
+          Don't have an account? <Text className="font-bold">Sign up </Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
